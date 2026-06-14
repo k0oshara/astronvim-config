528c5971ede4eef8356d08fa43bd6a1975ef56e2
k0oshara <mi9992828@gmail.com>
2026-06-15 01:19:50 +0300

    фон теплее и менне контрастнее

--- lua/plugins/gruvbox_material.lua
+++ lua/plugins/gruvbox_material.lua
@@ -6,7 +6,16 @@ return {
     init = function()
       vim.o.background = "dark"
       vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_foreground = "material"
+
+      vim.g.gruvbox_material_ui_contrast = "low"
+
+      vim.g.gruvbox_material_float_style = "dim"
+
+      vim.g.gruvbox_material_enable_bold = 0
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.g.gruvbox_material_disable_italic_comment = 1
+
       vim.g.gruvbox_material_better_performance = 0
     end,
   },
@@ -16,16 +25,20 @@ return {
     opts = {
       colorscheme = "gruvbox-material",
       highlights = {
-        init = {
-          Normal = { bg = "#1b1b1b" },
-          NormalNC = { bg = "#1b1b1b" },
-          SignColumn = { bg = "#1b1b1b" },
-          EndOfBuffer = { bg = "#1b1b1b" },
-          FloatBorder = { bg = "#1b1b1b" },
-          NormalFloat = { bg = "#1b1b1b" },
-          WinBar = { bg = "#1b1b1b" },
-          WinBarNC = { bg = "#1b1b1b" },
-        },
+      init = function()
+        local bg = "#1b1816"
+
+        return {
+          Normal = { bg = bg },
+          NormalNC = { bg = bg },
+          SignColumn = { bg = bg },
+          EndOfBuffer = { bg = bg },
+          FloatBorder = { bg = bg },
+          NormalFloat = { bg = bg },
+          WinBar = { bg = bg },
+          WinBarNC = { bg = bg },
+        }
+      end,
       },
     },
   },
